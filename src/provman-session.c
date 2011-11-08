5acf0257fc69331b40b9441c4552e1406867f08e
Mark Ryan <mark.d.ryan@intel.com>
2011-11-08 10:29:38 +0100

    [PROVMAN] The names of session log files now contain the user name.
    This is to prevent two instances of the provman-session process running
    on two different session buses from overwriting each other's log files.

--- src/provman-session.c
+++ src/provman-session.c
@@ -30,9 +30,30 @@
  ******************************************************************************/
 
 #include "config.h"
+
+#include <glib.h>
+
 #include "provman.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])     
 {
-	return provman_run(G_BUS_TYPE_SESSION, PROVMAN_SESSION_LOG);
+	int retval;
+	const char *log_name = NULL;
+
+#ifdef PROVMAN_LOGGING
+	GString *log_name_str;
+
+	log_name_str = g_string_new(PROVMAN_SESSION_LOG);
+	g_string_append(log_name_str, g_get_user_name());
+	g_string_append(log_name_str, ".log");
+	log_name = log_name_str->str;
+#endif
+
+	retval = provman_run(G_BUS_TYPE_SESSION, log_name);
+
+#ifdef PROVMAN_LOGGING
+	(void) g_string_free(log_name_str, TRUE);
+#endif
+
+	return retval;
 }
